e7b1d5133b75039afad03b17efb849c7d43f56b6
anasilva65 <annadasilva89@hotmail.com>
2021-02-23 19:24:45 -0300

    teste do primeiro metodo

--- elixirschool/identicon/test/identicon_test.exs
+++ elixirschool/identicon/test/identicon_test.exs
@@ -2,7 +2,8 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  test "teste do metodo principal, antes de dividir a lista em sublistas" do
+    hash = Identicon.main("banana")
+    assert hash != [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124]
   end
 end
