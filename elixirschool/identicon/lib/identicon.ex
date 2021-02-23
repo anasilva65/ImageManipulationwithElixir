46d5cf799f2070f4071ee031c493eb86b2b2e931
anasilva65 <annadasilva89@hotmail.com>
2021-02-23 16:33:49 -0300

    recebendo o modulo enum, chamando a funcao chunk...

--- elixirschool/identicon/lib/identicon.ex
+++ elixirschool/identicon/lib/identicon.ex
@@ -6,13 +6,12 @@ defmodule Identicon do
     |> build_grid
   end
 
-  def build_grid( %Identicon.Image{hex: hex}) do
-
+  def build_grid( %Identicon.Image{hex: hex} = image) do
+  hex
+  |> Enum.chunk(3)
   end
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-
   %Identicon.Image{image | color: {r, g, b}}
-
   end
 
   def hash_input(input) do
