36397d55590c16be59a01793e0347856801a087f
anasilva65 <annadasilva89@hotmail.com>
2021-02-24 16:53:16 -0300

    apenas a organizacao do codigo, oas mensagens dos outros commits permanecem

--- elixirschool/identicon/lib/identicon.ex
+++ elixirschool/identicon/lib/identicon.ex
@@ -1,4 +1,7 @@
 defmodule Identicon do
+  @moduledoc """
+  Takes in a string and returns an identicon image.
+  """
   def main(input) do
     input
     |> hash_input
@@ -6,18 +9,18 @@ defmodule Identicon do
     |> build_grid
   end
 
-  def build_grid( %Identicon.Image{hex: hex} = image) do
-  hex
-  |> Enum.chunk(3)
-  end
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-  %Identicon.Image{image | color: {r, g, b}}
-  end
-
   def hash_input(input) do
-  hex = :crypto.hash(:md5, input)
-  |> :binary.bin_to_list
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
 
-  %Identicon.Image{hex: hex}
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+      hex
+      |> Enum.chunk(3)
   end
 end
